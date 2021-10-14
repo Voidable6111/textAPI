7a7f5064b01f17bf51dd0d5b15a89e705b11017c
Cole Dickson <coldixon7@gmail.com>
2021-10-14 12:35:58 -0700

    nearly done, the second to last push.

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-//import dependencies
+// import dependencies
 var express = require('express');
 var app = express();
 var apiRoutes = require('./routes/api');
 var rootRoutes = require('./routes/root');
-var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
 
-//(server/middleware)
+// setup our app (server/middleware) 
 app.use(express.urlencoded({
     extended: true
 }));
 
 app.use(express.json());
 
-function logAccess(req, res, next){
-    console.log(req.method, req.originalUrl);
-    next();
-}
-app.use(logAccess);
+app.use(morgan('combined'));
 
-// more endpoints
+
+// add some endpoints
+
 app.use('/', rootRoutes);
 app.use('/api', apiRoutes);
 
-//serve out app
-var server = app.listen(8080, function(){
+// serve out our app
+var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log("Example app listening on", host, port);
+    console.log("Example app listening on ", host, port);
 });
--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,150 @@
-//dependencies
+// dependencies
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
-var time = new Date();
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
+const fs = require('fs');
 
+// --------------------------------------------------------- Endpoints/Routes
 
-//endpoints
-router.get('/', function(req, res){
-    time.getMonth();
-    time.getDay();
-    time.getHours();
-    time.getMinutes();
-    res.status(200).json({message: "read all." + "( [" + time.getHours() + ":" + time.getMinutes() + "] " + monthNames[time.getMonth()] + " " + time.getDate() +", " +  time.getFullYear() +")"});
+// CRUD - Create, Read, Update, Delete
+
+// get all of a resource - Read
+router.get('/', function(req, res) {
+    try {
+        const rawdata = fs.readFileSync('data.json'); // <Buffer <hex code>
+        var messages = JSON.parse(rawdata);
     
+        console.log(messages);
+        /*messages = messages.filter((val) =>{
+            return val.text;
+        })*/
+    
+        res.status(200).json(messages);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
-router.post('/', function(req, res){
-    res.status(201).json({message: "create"});
+
+// get one of a resource - Read
+router.get('/:id', function(req, res) {
+    try {
+        const rawdata = fs.readFileSync('data.json'); // <Buffer <hex code>
+        var messages = JSON.parse(rawdata);
+        messages[req.params.id].create_on = Date.now();
+        res.status(200).json(messages[req.params.id].name + ": " + messages[req.params.id].text + ", created: " + messages[req.params.id].create_on);
+        messages[req.params.id].update_on = Date.now();
+        //FIGURE TIMESTAMPS OUT HERE. THEN DO THE GET ABOVE WITHOUT AN ID, THEN PLAY CLEANUP BELOW. PRETTY MUCH IT.
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
-router.patch('/:id', function(req, res){
-    res.status(200).json({message: "edited"});
+
+// create a new resource - Create
+router.post('/', function(req, res) {
+    try {
+        console.log("Posted Object is: ", req.body);
+        // open the file
+        const rawdata = fs.readFileSync('data.json');
+        // decode the file (parse) so we can use it
+        var messages = JSON.parse(rawdata);
+
+        // add data, but controlled
+        var rawBody = req.body;
+
+        var newObj = {
+            name: null,
+            age: null,
+            currentGame: null
+        };
+
+        if (rawBody.name != null) {
+            newObj.name = rawBody.name;
+        }
+        
+        if (rawBody.age != null) {
+            newObj.age = rawBody.age;
+        }
+        
+        if (rawBody.currentGame != null) {
+            newObj.currentGame = rawBody.currentGame;
+        }
+        
+
+
+        // get the actual index
+        newObj._id = messages.length;
+
+
+        // add our new object to the array
+        messages.push(newObj);
+
+        // save (write) the data back to the file
+        const data = fs.writeFileSync('data.json', JSON.stringify(messages));
+
+        // return the data to the user
+        res.status(201).json(newObj);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
-router.delete('/:id', function(req, res){
-    res.status(200).json({message: "delete this"});
+
+// updated a resource - Update
+router.patch('/:id', function(req, res) {
+    try {
+        console.log("Object being patched is: ", req.params.id, req.body);
+        // open the file
+        const rawdata = fs.readFileSync('data.json');
+        // decode the file (parse) so we can use it
+        var messages = JSON.parse(rawdata);
+
+        // add data, but controlled
+        var id = req.params.id;
+        var rawBody = req.body;
+
+        if (rawBody.name != null) {
+            messages[id].name = rawBody.name;
+        }
+        
+        if (rawBody.age != null) {
+            messages[id].age = rawBody.age;
+        }
+        
+        if (rawBody.currentGame != null) {
+            messages[id].currentGame = rawBody.currentGame;
+        }
+
+        // save (write) the data back to the file
+        const data = fs.writeFileSync('data.json', JSON.stringify(messages));
+
+        // return the data to the user
+        res.status(200).json(messages[id]);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
+
+// delete a resource - Delete
+router.delete('/:id', function(req, res) {
+    // capture the id
+    var id = req.params.id;
+
+    // open the file for reading
+    const rawdata = fs.readFileSync('data.json'); // <Buffer <hex code>
+    var messages = JSON.parse(rawdata);
+
+    // if found delete it
+    if (messages.length > id) {
+        // modify the object
+        messages.splice(id, 1);
+
+        // write to the file
+        const data = fs.writeFileSync('data.json', JSON.stringify(messages));
+
+        res.status(200).json({ message: "ok" });
+    } else {
+        res.status(500).json({ message: "Something went wrong" });
+    }    
+});
+
+// ----------------------------------------------------------------- end routes/endpoints
 
 module.exports = router;
